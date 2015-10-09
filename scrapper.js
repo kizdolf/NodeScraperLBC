cc6c5ae88df8885757a22e40dd843ce5d234b483
jburet <j.buret@mektoube.fr>
2015-10-09 12:44:02 +0200

    suck this flat. request each flat. and suck them.

--- scrapper.js
+++ scrapper.js
@@ -14,7 +14,16 @@ send_mail = function(apparts){
 	var plural = (apparts.length > 1) ? ' appartements' : ' appartement', //funk. or Useless?
 	html = "<ul>";
 	apparts.forEach(function(one){
-		html += '<li style="line-height:24px; font-size:18px;">' + one.price + ' : <a href="' + one.href + '">' +  one.title + '</a></li><br>';
+		html += '<li><span style="line-height:24px; font-size:18px;">' + one.price + ' : <a href="' + one.href + '">' +  one.title + '</a></span>';
+		if(one.labels){
+			html += '<small><ul>';
+			one.labels.forEach(function(l, i){
+				l = l.replace(new RegExp('�', 'g'), 'é'); //jshint ignore:line
+				html+= '<li>' + l + one.values[i] + '</li>';
+			});
+			html += '</ul></small>';
+		}
+		html += '</li><br>';
 	});
 	html += '</ul>';
 	transporter.sendMail({
@@ -26,6 +35,43 @@ send_mail = function(apparts){
 	    if(error) console.log(error);
 	    else console.log('Message sent: ' + info.response + ' à ' + new Date());
 	});
+},
+
+suck_this_flat = function(link, price, cb){
+ 	var href = link.attribs.href;
+	var id_offre = href.split('/')[4].split('.')[0];
+	if(href.indexOf('locations') !== -1 && !ids[id_offre]){
+		var flat = {};
+		request({uri: href, encoding: null}, function(err, resp, body){
+			if(err){
+				return cb(err, false);
+			}else{
+				flat.price = price;
+				flat.href = href;
+				flat.title = link.attribs.title;
+				flat.labels = [];
+				flat.values = [];
+
+				var $ = cheerio.load(body);
+				var crits = $('.criterias');
+				flat.desc = $('.content').html();
+				if(crits.html() !== null){
+					var criteres = cheerio.load(crits.html());
+					var labels = criteres('tr').find('th');
+					var values = criteres('tr').find('td');
+					labels.each(function(i, label){
+						flat.labels.push(label.children[0].data);
+					});
+					values.each(function(i, val){
+						flat.values.push(val.children[0].data);
+					});
+				}
+				return cb(false, flat);
+			}
+		});
+	}else{
+		return cb(false, false);
+	}
 },
 scrap = function(){
 	request({uri: conf.url_offres}, function(error, response, body) {
@@ -39,17 +85,19 @@ scrap = function(){
 			prices.each(function(i, price){
 				prix.push(price.children[0].data.trim());
 			});
+			var c = links.length;
 			links.each(function(i, link){
-				var util 	= link.attribs,
-				id 			= util.href.split('/')[4].split('.')[0];
-				util.price 	= prix[i]; //href and prices have the same index. Easy :)
-				if(util.href.indexOf('locations') !== -1 && !ids[id]){
-					apparts.push(util);
-					ids[id] = 1;
-				}
+				suck_this_flat(link, prix[i], function(err, flat){
+					c--;
+					if(!err && flat !== false){
+						apparts.push(flat);
+						if(!c){
+							if(apparts.length > 0) send_mail(apparts);
+							else console.log(new Date() + ' Rien de nouveau à envoyer.');
+						}
+					}else console.log(err + ' < is an errror');
+				});
 			});
-			if(apparts.length > 0) send_mail(apparts);
-			else console.log(new Date() + ' Rien de nouveau à envoyer.');
 		}else console.log('error requesting body page : ' + error);
 	});
 };
