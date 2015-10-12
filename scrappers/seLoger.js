b14eeae633d2c0b241612aba47d3903f6fbc2406
kizdolf <jules.buret@gmail.com>
2015-10-12 13:54:01 +0200

    more infos.

--- scrappers/seLoger.js
+++ scrappers/seLoger.js
@@ -5,6 +5,22 @@ request 			= require('request'),
 cheerio 			= require('cheerio'), //parse it like Jquery
 iconv 				= require('iconv-lite');
 
+var suck_it = function(url, cb){
+  request({uri: url, encoding: null}, function(err, resp, body){
+    if(err) return cb([], []);
+    var values = [];
+    var labels = [];
+    body = iconv.decode(body, 'utf8');
+    var $ = cheerio.load(body);
+    var props = $('.description-liste').find('li');
+    props.each(function(){
+      labels.push(' ');
+      values.push($(this).html());
+    });
+    return cb(labels, values);
+  });
+};
+
 exports.scrap = function(url){
   return new Promise(function(resolve, reject){
     var apparts = [];
@@ -14,25 +30,27 @@ exports.scrap = function(url){
       body = iconv.decode(body, 'utf8');
       var $ = cheerio.load(body);
       var list = $('.liste_resultat').find('article');
+      var count = list.length;
       list.each(function(){
         var infos = $(this).find('.listing_infos');
-        var props = $(this).find('.property_list');
-
         var flat = {from: 'seLoger', labels:[], values:[], upload: false};
         flat.id = parseInt($(this).attr('data-listing-id'));
         flat.href = $($(infos).find('a')).attr('href');
         flat.price = $($(infos).find('.amount')).html();
         flat.title = $($(infos).find('a')).attr('title');
-        $(props).find('li').each(function(){
-          flat.labels.push(' ');
-          flat.values.push($(this).html());
-        });
-        if(!db('seLoger').find({id: flat.id})){
-          db('seLoger').push({id: flat.id});
-          apparts.push(flat);
-        }
+          suck_it(flat.href, function(labels, values){
+            count--;
+            if(!db('seLoger').find({id: flat.id})){
+              db('seLoger').push({id: flat.id});
+              flat.labels = labels;
+              flat.values = values;
+              apparts.push(flat);
+            }
+            if(count === 0){
+              resolve(apparts);
+            }
+          });
       });
-      resolve(apparts);
     });
   });
 };
